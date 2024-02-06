95ce8aac08bb3bf200c5779df812139e9fc0889f
dilipkumarmehta <dilipkumarmehta02@gmail.com>
2024-02-07 00:13:12 +0530

    interceptor

---
+++ src/main/java/com/dilip/intercepter/RequesthandelerInterceptor.java
@@ -0,0 +1,20 @@
+package com.dilip.intercepter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class RequesthandelerInterceptor extends HandlerInterceptorAdapter {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("interceptor get call");
+		if (true) {
+			response.setContentType("text/html");
+			response.getWriter().println("Getting response from interceptor not forwardign the request to controller");
+			return false;
+		}
+		return super.preHandle(request, response, handler);
+	}
+}
