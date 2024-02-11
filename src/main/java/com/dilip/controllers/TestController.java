56b979955bf43daecc1c66fd2f86772615c798ff
dilipkumarmehta <dilipkumarmehta02@gmail.com>
2024-02-11 15:49:34 +0530

    condif

--- src/main/java/com/dilip/controllers/TestController.java
+++ src/main/java/com/dilip/controllers/TestController.java
@@ -4,16 +4,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.dilip.config.AppConfig;
 
 /**
  * http://localhost:8080/spring-mvc/index http://localhost:8080/spring-mvc/home
  */
 @Controller
 public class TestController {
+
+	@Autowired
+	AppConfig appConfig;
+
+	@Value("${b2c.clientId}")
+	public String clientID;
 
 	@RequestMapping(value = "/home")
 	public String home() {
@@ -25,6 +35,7 @@ public class TestController {
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public ModelAndView index() {
 		System.out.println("index call");
+		System.out.println("b2c.clientId " + clientID);
 		return new ModelAndView("index");
 
 	}
